316fb26248a5e99e8af5eeddfd7887299d23a19e
berlinau@yahoo.com <berlinbernadin@gmail.com>
2023-01-04 21:09:00 +0000

    exercise 4 solution

---
+++ src/main/java/com/serenitydojo/Dog.java
@@ -0,0 +1,41 @@
+package com.serenitydojo;
+
+public class Dog {
+    private String name;
+    private String favoriteToy;
+    private int age;
+    private boolean isFed;
+
+    static String DOG_NOISE = "Woof";
+
+    public Dog(String name, String favoriteToy, int age) {
+        this.name = name;
+        this.favoriteToy = favoriteToy;
+        this.age = age;
+        this.isFed = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFavoriteToy() {
+        return favoriteToy;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String makeNoise() {
+        return DOG_NOISE;
+    }
+
+    public void feed() {
+        isFed = true;
+    }
+
+    public boolean isFed() {
+        return isFed;
+    }
+}
